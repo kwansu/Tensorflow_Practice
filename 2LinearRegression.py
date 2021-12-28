a1ee0008e9dcc3925e881a54cf9175b06d432833
kwansu <kwansu91@naver.com>
2021-12-28 23:38:42 +0900

    sks

--- 2LinearRegression.py
+++ 2LinearRegression.py
@@ -1,14 +1,17 @@
-import numpy
-import tensorflow
+import numpy as np
+import tensorflow.keras.models as models
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import SGD
 
 x_train = [1, 2, 3, 4]
 y_train = [2, 0, -2, -4]
 
-model = tensorflow.keras.models.Sequential([tensorflow.keras.layers.Dense(1, activation='linear')])
+model = models.Sequential()
+model.add(Dense(1, activation='linear'))
 
-model.compile(optimizer=tensorflow.keras.optimizers.SGD(learning_rate=0.1), loss='mse')
+model.compile(optimizer=SGD(learning_rate=0.1), loss='mse')
 
-model.fit(numpy.array(x_train, numpy.float), numpy.array(y_train, numpy.float), epochs=1000)
+model.fit(np.array(x_train, np.float), np.array(y_train, np.float), epochs=1000)
 
 predict = model.predict([-2, 7])
 print(predict)
--- 3.LinearRegression_multiVariable.py
+++ 3.LinearRegression_multiVariable.py
@@ -1,20 +1,22 @@
-import numpy
-import tensorflow
+import numpy as np
+import tensorflow.keras.models as models
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import SGD
 
-x_data = numpy.array([[73., 80., 75.],
-                      [93., 88., 93.],
-                      [89., 91., 90.],
-                      [96., 98., 100.],
-                      [73., 66., 70.]], numpy.float)
+x_data = np.array([[73., 80., 75.],
+                    [93., 88., 93.],
+                    [89., 91., 90.],
+                    [96., 98., 100.],
+                    [73., 66., 70.]])
 
-y_data = numpy.array([152., 185., 180., 196., 142.], numpy.float)
+y_data = np.array([152., 185., 180., 196., 142.])
 
-model = tensorflow.keras.models.Sequential(
-    [tensorflow.keras.layers.Dense(1, activation='linear')])
+model = models.Sequential()
+model.add(Dense(1, activation='linear'))
 
-model.compile(optimizer=tensorflow.keras.optimizers.SGD(learning_rate=0.00001), loss='mse')
+model.compile(optimizer=SGD(learning_rate=0.00001), loss='mse')
 
 model.fit(x_data, y_data, epochs=1000)
 
-predict = model.predict(numpy.array([[100, 100, 80]]))
+predict = model.predict(np.array([[100, 100, 80]]))
 print(predict)
